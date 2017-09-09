e114ff404726a65f569aa1d84b7c79ce257e02d0
altusgerona <alecgerona@gmail.com>
2017-09-09 20:18:56 +0800

    Add cetaphil, solmux, gynepro, and ritemed

---
+++ app/imagecollection.js
@@ -0,0 +1,488 @@
+var imageCollection = {
+  advil : {
+    part1 : {
+      name : "advil1part1",
+      src : "images/advil/advil-1.jpg"
+    },
+    part2 : {
+      name : "advil1part2",
+      src : "images/advil/advil-2.jpg"
+    },
+    part3 : {
+      name : "advil1part3",
+      src : "images/advil/advil-3.jpg"
+    },
+    part4 : {
+      name : "advil1part4",
+      src : "images/advil/advil-4.jpg"
+    }
+  },
+  alaxan : {
+    part1 : {
+      name : "alaxan1part1",
+      src : "images/alaxan/alaxan-1.jpg"
+    },
+    part2 : {
+      name : "alaxan1part2",
+      src : "images/alaxan/alaxan-2.jpg"
+    },
+    part3 : {
+      name : "alaxan1part3",
+      src : "images/alaxan/alaxan-3.jpg"
+    },
+    part4 : {
+      name : "alaxan1part4",
+      src : "images/alaxan/alaxan-4.jpg"
+    }
+  },
+  allerta : {
+    part1 : {
+      name : "allerta1part1",
+      src : "images/allerta/allerta-1.jpg"
+    },
+    part2 : {
+      name : "allerta1part2",
+      src : "images/allerta/allerta-2.jpg"
+    },
+    part3 : {
+      name : "allerta1part3",
+      src : "images/allerta/allerta-3.jpg"
+    },
+    part4 : {
+      name : "allerta1part4",
+      src : "images/allerta/allerta-4.jpg"
+    }
+  },
+  ascof : {
+    part1 : {
+      name : "ascof1part1",
+      src : "images/ascof/ascof-1.jpg"
+    },
+    part2 : {
+      name : "ascof1part2",
+      src : "images/ascof/ascof-2.jpg"
+    },
+    part3 : {
+      name : "ascof1part3",
+      src : "images/ascof/ascof-3.jpg"
+    },
+    part4 : {
+      name : "ascof1part4",
+      src : "images/ascof/ascof-4.jpg"
+    }
+  },
+  bioflu : {
+    part1 : {
+      name : "bioflu1part1",
+      src : "images/bioflu/bioflu-1.jpg"
+    },
+    part2 : {
+      name : "bioflu1part2",
+      src : "images/bioflu/bioflu-2.jpg"
+    },
+    part3 : {
+      name : "bioflu1part3",
+      src : "images/bioflu/bioflu-3.jpg"
+    },
+    part4 : {
+      name : "bioflu1part4",
+      src : "images/bioflu/bioflu-4.jpg"
+    }
+  },
+  biogesic : {
+    part1 : {
+      name : "biogesic1part1",
+      src : "images/biogesic/biogesic-1.jpg"
+    },
+    part2 : {
+      name : "biogesic1part2",
+      src : "images/biogesic/biogesic-2.jpg"
+    },
+    part3 : {
+      name : "biogesic1part3",
+      src : "images/biogesic/biogesic-3.jpg"
+    },
+    part4 : {
+      name : "biogesic1part4",
+      src : "images/biogesic/biogesic-4.jpg"
+    }
+  },
+  celeteque : {
+    part1 : {
+      name : "celeteque1part1",
+      src : "images/celeteque/celeteque-1.jpg"
+    },
+    part2 : {
+      name : "celeteque1part2",
+      src : "images/celeteque/celeteque-2.jpg"
+    },
+    part3 : {
+      name : "celeteque1part3",
+      src : "images/celeteque/celeteque-3.jpg"
+    },
+    part4 : {
+      name : "celeteque1part4",
+      src : "images/celeteque/celeteque-4.jpg"
+    }
+  },
+  centrum : {
+    part1 : {
+      name : "centrum1part1",
+      src : "images/centrum/centrum-1.jpg"
+    },
+    part2 : {
+      name : "centrum1part2",
+      src : "images/centrum/centrum-2.jpg"
+    },
+    part3 : {
+      name : "centrum1part3",
+      src : "images/centrum/centrum-3.jpg"
+    },
+    part4 : {
+      name : "centrum1part4",
+      src : "images/centrum/centrum-4.jpg"
+    }
+  },
+  conzace : {
+    part1 : {
+      name : "conzace1part1",
+      src : "images/conzace/conzace-1.jpg"
+    },
+    part2 : {
+      name : "conzace1part2",
+      src : "images/conzace/conzace-2.jpg"
+    },
+    part3 : {
+      name : "conzace1part3",
+      src : "images/conzace/conzace-3.jpg"
+    },
+    part4 : {
+      name : "conzace1part4",
+      src : "images/conzace/conzace-4.jpg"
+    }
+  },
+  decolgen : {
+    part1 : {
+      name : "decolgen1part1",
+      src : "images/decolgen/decolgen-1.jpg"
+    },
+    part2 : {
+      name : "decolgen1part2",
+      src : "images/decolgen/decolgen-2.jpg"
+    },
+    part3 : {
+      name : "decolgen1part3",
+      src : "images/decolgen/decolgen-3.jpg"
+    },
+    part4 : {
+      name : "decolgen1part4",
+      src : "images/decolgen/decolgen-4.jpg"
+    }
+  },
+  diatabs : {
+    part1 : {
+      name : "diatabspart1",
+      src : "images/diatabs/diatabs-1.jpg"
+    },
+    part2 : {
+      name : "diatabspart2",
+      src : "images/diatabs/diatabs-2.jpg"
+    },
+    part3 : {
+      name : "diatabspart3",
+      src : "images/diatabs/diatabs-3.jpg"
+    },
+    part4 : {
+      name : "diatabspart4",
+      src : "images/diatabs/diatabs-4.jpg"
+    }
+  },
+  efficascent : {
+    part1 : {
+      name : "efficascentpart1",
+      src : "images/efficascent/efficascent-1.jpg"
+    },
+    part2 : {
+      name : "efficascentpart2",
+      src : "images/efficascent/efficascent-2.jpg"
+    },
+    part3 : {
+      name : "efficascentpart3",
+      src : "images/efficascent/efficascent-3.jpg"
+    },
+    part4 : {
+      name : "efficascentpart4",
+      src : "images/efficascent/efficascent-4.jpg"
+    }
+  },
+  enervon : {
+    part1 : {
+      name : "enervonpart1",
+      src : "images/enervon/enervon-1.jpg"
+    },
+    part2 : {
+      name : "enervonpart2",
+      src : "images/enervon/enervon-2.jpg"
+    },
+    part3 : {
+      name : "enervonpart3",
+      src : "images/enervon/enervon-3.jpg"
+    },
+    part4 : {
+      name : "enervonpart4",
+      src : "images/enervon/enervon-4.jpg"
+    }
+  },
+  gatorade : {
+    part1 : {
+      name : "gatoradepart1",
+      src : "images/gatorade/gatorade-1.jpg"
+    },
+    part2 : {
+      name : "gatoradepart2",
+      src : "images/gatorade/gatorade-2.jpg"
+    },
+    part3 : {
+      name : "gatoradepart3",
+      src : "images/gatorade/gatorade-3.jpg"
+    },
+    part4 : {
+      name : "gatoradepart4",
+      src : "images/gatorade/gatorade-4.jpg"
+    }
+  },
+  kremils : {
+    part1 : {
+      name : "kremilspart1",
+      src : "images/kremils/kremils-1.jpg"
+    },
+    part2 : {
+      name : "kremilspart2",
+      src : "images/kremils/kremils-2.jpg"
+    },
+    part3 : {
+      name : "kremilspart3",
+      src : "images/kremils/kremils-3.jpg"
+    },
+    part4 : {
+      name : "kremilspart4",
+      src : "images/kremils/kremils-4.jpg"
+    }
+  },
+  lactacyd : {
+    part1 : {
+      name : "lactacydpart1",
+      src : "images/lactacyd/lactacyd-1.jpg"
+    },
+    part2 : {
+      name : "lactacydpart2",
+      src : "images/lactacyd/lactacyd-2.jpg"
+    },
+    part3 : {
+      name : "lactacydpart3",
+      src : "images/lactacyd/lactacyd-3.jpg"
+    },
+    part4 : {
+      name : "lactacydpart4",
+      src : "images/lactacyd/lactacyd-4.jpg"
+    }
+  },
+  mcdo : {
+    part1 : {
+      name : "mcdopart1",
+      src : "images/mcdo/mcdo-1.jpg"
+    },
+    part2 : {
+      name : "mcdopart2",
+      src : "images/mcdo/mcdo-2.jpg"
+    },
+    part3 : {
+      name : "mcdopart3",
+      src : "images/mcdo/mcdo-3.jpg"
+    },
+    part4 : {
+      name : "mcdopart4",
+      src : "images/mcdo/mcdo-4.jpg"
+    }
+  },
+  myra : {
+    part1 : {
+      name : "myrapart1",
+      src : "images/myra/myra-1.jpg"
+    },
+    part2 : {
+      name : "myrapart2",
+      src : "images/myra/myra-2.jpg"
+    },
+    part3 : {
+      name : "myrapart3",
+      src : "images/myra/myra-3.jpg"
+    },
+    part4 : {
+      name : "myrapart4",
+      src : "images/myra/myra-4.jpg"
+    }
+  },
+  neozep : {
+    part1 : {
+      name : "neozeppart1",
+      src : "images/neozep/neozep-1.jpg"
+    },
+    part2 : {
+      name : "neozeppart2",
+      src : "images/neozep/neozep-2.jpg"
+    },
+    part3 : {
+      name : "neozeppart3",
+      src : "images/neozep/neozep-3.jpg"
+    },
+    part4 : {
+      name : "neozeppart4",
+      src : "images/neozep/neozep-4.jpg"
+    }
+  },
+  phcare : {
+    part1 : {
+      name : "phcarepart1",
+      src : "images/phcare/phcare-1.jpg"
+    },
+    part2 : {
+      name : "phcarepart2",
+      src : "images/phcare/phcare-2.jpg"
+    },
+    part3 : {
+      name : "phcarepart3",
+      src : "images/phcare/phcare-3.jpg"
+    },
+    part4 : {
+      name : "phcarepart4",
+      src : "images/phcare/phcare-4.jpg"
+    }
+  },
+  rexidol : {
+    part1 : {
+      name : "rexidolpart1",
+      src : "images/rexidol/rexidol-1.jpg"
+    },
+    part2 : {
+      name : "rexidolpart2",
+      src : "images/rexidol/rexidol-2.jpg"
+    },
+    part3 : {
+      name : "rexidolpart3",
+      src : "images/rexidol/rexidol-3.jpg"
+    },
+    part4 : {
+      name : "rexidolpart4",
+      src : "images/rexidol/rexidol-4.jpg"
+    }
+  },
+  robitussin : {
+    part1 : {
+      name : "robitussinpart1",
+      src : "images/robitussin/robitussin-1.jpg"
+    },
+    part2 : {
+      name : "robitussinpart2",
+      src : "images/robitussin/robitussin-2.jpg"
+    },
+    part3 : {
+      name : "robitussinpart3",
+      src : "images/robitussin/robitussin-3.jpg"
+    },
+    part4 : {
+      name : "robitussinpart4",
+      src : "images/robitussin/robitussin-4.jpg"
+    }
+  },
+  skelan : {
+    part1 : {
+      name : "skelanpart1",
+      src : "images/skelan/skelan-1.jpg"
+    },
+    part2 : {
+      name : "skelanpart2",
+      src : "images/skelan/skelan-2.jpg"
+    },
+    part3 : {
+      name : "skelanpart3",
+      src : "images/skelan/skelan-3.jpg"
+    },
+    part4 : {
+      name : "skelanpart4",
+      src : "images/skelan/skelan-4.jpg"
+    }
+  },
+  tgp : {
+    part1 : {
+      name : "tgppart1",
+      src : "images/tgp/tgp-1.jpg"
+    },
+    part2 : {
+      name : "tgppart2",
+      src : "images/tgp/tgp-2.jpg"
+    },
+    part3 : {
+      name : "tgppart3",
+      src : "images/tgp/tgp-3.jpg"
+    },
+    part4 : {
+      name : "tgppart4",
+      src : "images/tgp/tgp-4.jpg"
+    }
+  },
+  uhp : {
+    part1 : {
+      name : "uhppart1",
+      src : "images/uhp/uhp-1.jpg"
+    },
+    part2 : {
+      name : "uhppart2",
+      src : "images/uhp/uhp-2.jpg"
+    },
+    part3 : {
+      name : "uhppart3",
+      src : "images/uhp/uhp-3.jpg"
+    },
+    part4 : {
+      name : "uhppart4",
+      src : "images/uhp/uhp-4.jpg"
+    }
+  },
+  unilab : {
+    part1 : {
+      name : "unilabpart1",
+      src : "images/unilab/unilab-1.jpg"
+    },
+    part2 : {
+      name : "unilabpart2",
+      src : "images/unilab/unilab-2.jpg"
+    },
+    part3 : {
+      name : "unilabpart3",
+      src : "images/unilab/unilab-3.jpg"
+    },
+    part4 : {
+      name : "unilabpart4",
+      src : "images/unilab/unilab-4.jpg"
+    }
+  },
+  vicks : {
+    part1 : {
+      name : "vickspart1",
+      src : "images/vicks/vicks-1.jpg"
+    },
+    part2 : {
+      name : "vickspart2",
+      src : "images/vicks/vicks-2.jpg"
+    },
+    part3 : {
+      name : "vickspart3",
+      src : "images/vicks/vicks-3.jpg"
+    },
+    part4 : {
+      name : "vickspart4",
+      src : "images/vicks/vicks-4.jpg"
+    }
+  }
+}
