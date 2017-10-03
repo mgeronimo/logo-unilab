0b70a1a9bbfbdd43c818a2bcc11c6aecfea79506
Michael Taquiqui <mareal29@gmail.com>
2017-10-03 21:38:00 +0800

    update total report

--- app/tally-reports/total.js
+++ app/tally-reports/total.js
@@ -2,7 +2,7 @@ window.onload = function() {
 
     var advilCounter = 0;
     var ref2 = firebase.database().ref().child("users/");
-    ref.once("value").then(function(snapshot) {
+    ref2.once("value").then(function(snapshot) {
         console.log(snapshot.val().length);
         if(snapshot.hasChild('advil')) advilCounter++;
     });
